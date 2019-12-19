bab45ac146539faa1ef389d02e8a699952e19e69
yvangak <gakyvan@gmail.com>
2019-12-18 23:17:24 -0500

    first commit: initialized the project

---
+++ spring-batch-nba-chunk-demo/src/main/java/com/yvangak/batch/chunk/NbaChunkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yvangak.batch.chunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaChunkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaChunkDemoApplication.class, args);
+    }
+
+}
---
+++ spring-batch-nba-chunk-demo/src/test/java/com/yvangak/batch/chunk/NbachunckdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvangak.batch.chunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbachunckdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
