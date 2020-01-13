244a6623e772ba427b7a58a5377475eb0c2fa83e
yvangak <gakyvan@gmail.com>
2020-01-13 16:18:05 -0500

    added batch config class that loads different beans

--- spring-batch-nba-chunk-demo/src/main/java/com/yvangak/batch/chunk/NbaChunkDemoApplication.java
+++ spring-batch-nba-chunk-demo/src/main/java/com/yvangak/batch/chunk/NbaChunkDemoApplication.java
@@ -1,5 +1,4 @@
 package com.yvangak.batch.chunk;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ spring-batch-nba-chunk-demo/src/main/java/com/yvangak/batch/chunk/config/BatchChunkConfig.java
@@ -0,0 +1,51 @@
+package com.yvangak.batch.chunk.config;
+
+import com.yvangak.batch.chunk.batch.FranchiseProcessor;
+import com.yvangak.batch.chunk.batch.FranchiseReader;
+import com.yvangak.batch.chunk.batch.FranchiseWriter;
+import com.yvangak.batch.chunk.clients.model.Franchise;
+import com.yvangak.batch.chunk.domain.FranchiseStats;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchChunkConfig {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+    @Autowired
+    private FranchiseReader franchiseReader;
+    @Autowired
+    private FranchiseProcessor franchiseProcessor;
+    @Autowired
+    private FranchiseWriter franchiseWriter;
+
+    @Bean
+    public Step processFranchise(ItemReader<Franchise> franchiseReader, ItemProcessor<Franchise,
+            FranchiseStats> franchiseProcessor, ItemWriter<FranchiseStats> franchiseWriter) {
+        return stepBuilderFactory.get("FranchiseStatsProcessor").<Franchise, FranchiseStats>chunk(5)
+                .reader(franchiseReader)
+                .processor(franchiseProcessor)
+                .writer(franchiseWriter)
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("FranchiseStatsChunkJob")
+                .start(processFranchise(franchiseReader, franchiseProcessor, franchiseWriter))
+                .build();
+    }
+
+}
